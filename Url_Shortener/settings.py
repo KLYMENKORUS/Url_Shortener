f643192650b081b3d87e81c4fe520b32cadcffff
Ruslan <klimenkoruslan@gmail.com>
2022-11-01 13:44:26 +0200

    Set DEBUG

--- Url_Shortener/settings.py
+++ Url_Shortener/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-^-_)xk4@6^_vd_u!hsncfy(*_68njjfrlx1amp50n^*g&q(4%v"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'url-shortener555.herokuapp.com']
 
 
 # Application definition
